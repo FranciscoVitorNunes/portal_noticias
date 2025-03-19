fd8f4e24eb80f6a1407050583580fca8e51bf11e
FranciscoVitorNunes <franciscovitornunes.programador@gmail.com>
2025-03-19 16:07:45 -0300

    criação do slider.

--- index.js
+++ index.js
@@ -1,33 +1,39 @@
 const express = require('express');
+var bodyParser = require('body-parser')
+
 const path = require('path');
-var bodyParser = require('body-parser');
 
 const app = express();
 
-app.use( bodyParser.json() );
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
 
-app.engine('html',require('ejs').renderFile );
+app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 app.use('/public', express.static(path.join(__dirname, 'public')));
-app.set('views', path.join(__dirname, '/views'));
+app.set('views', path.join(__dirname, '/pages'));
 
-app.get('/', (req,res)=>{
+
+app.get('/',(req,res)=>{
     
+    if(req.query.busca == null){
+        res.render('home',{});
+    }else{
+        res.send('Você buscou: '+req.query.busca);
+    }
+
+  
 });
 
-app.get('/:slug',(req, res)=>{
 
-});
-
-
-
-
+app.get('/:slug',(req,res)=>{
+    res.send(req.params.slug);
+})
 
 
 
 app.listen(5000,()=>{
-    console.log("Server online...");
-});
+    console.log('server rodando!');
+})
