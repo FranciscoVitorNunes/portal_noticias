69404c1d8764dff819379488306086b9bb2835b2
FranciscoVitorNunes <franciscovitornunes.programador@gmail.com>
2025-04-10 20:49:22 -0300

    Puxar posts do banco de dados.

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const path = require('path');
 
 const app = express();
 
-mongoose.connect('mongodb+srv://franvinu:Vito5757!@cluster0.nk6o00b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {useNewUrlParser: true, useUnifiedTopology: true}).then(function(){
+const Posts = require('./Posts.js');
+
+mongoose.connect('mongodb+srv://franvinu:Vito5757!@cluster0.nk6o00b.mongodb.net/portal_news?retryWrites=true&w=majority&appName=Cluster0', {useNewUrlParser: true, useUnifiedTopology: true}).then(function(){
     console.log('Conectado com  sucesso');
 }).catch(function(err){
     console.log("fala na conexão!!!");
@@ -24,15 +26,19 @@ app.use('/public', express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, '/pages'));
 
 
-app.get('/',(req,res)=>{
-    
-    if(req.query.busca == null){
-        res.render('home',{});
-    }else{
-        res.render('busca',{});
+app.get('/', async (req, res) => {
+    if (!req.query.busca) {
+        try {
+            const posts = await Posts.find({}).sort({ _id: -1 });
+            console.log(posts[0]);
+            res.render('home', { posts });
+        } catch (err) {
+            console.error(err);
+            res.status(500).send("Erro ao buscar posts");
+        }
+    } else {
+        res.render('busca', {});
     }
-
-  
 });
 
 
