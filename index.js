f77a48bf8b72524a61395acc4c8e6f09fadb3f10
FranciscoVitorNunes <franciscovitornunes.programador@gmail.com>
2025-04-10 18:21:35 -0300

    Conexão ao MongoDb Atlas.

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 var bodyParser = require('body-parser')
 
 const path = require('path');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://franvinu:Vito5757!@cluster0.nk6o00b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {useNewUrlParser: true, useUnifiedTopology: true}).then(function(){
+    console.log('Conectado com  sucesso');
+}).catch(function(err){
+    console.log("fala na conexão!!!");
+})
 
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
